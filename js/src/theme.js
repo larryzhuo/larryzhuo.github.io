fc8aae900208fc2f8f92628441afcac9edac462b
larryzhuo <1669790587@qq.com>
2017-08-20 20:59:11 +0800

    Site updated: 2017-08-20 20:59:10

--- js/src/theme.js
+++ js/src/theme.js
@@ -41,7 +41,7 @@
   this.Theme = Theme;
 
   //注册serviceWork
-  navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registeration){
+  navigator.serviceWorker && navigator.serviceWorker.register('./sw.js').then(function(registration){
     console.log('Excellent, registered with scope: ', registration.scope);
   })
 }.call(this));
